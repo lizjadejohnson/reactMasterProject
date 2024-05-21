78b70b2e2964a75eb8c5cec42733bddc82fbfc5e
Liz Johnson <liz.jade.johnson@gmail.com>
2024-05-21 10:08:14 -0400

    refactored code to be more modular

--- frontend/src/pages/NotesPage.jsx
+++ frontend/src/pages/NotesPage.jsx
@@ -3,16 +3,20 @@ import Note from '../components/Note'
 
 
 //The note._id is provided by Mongo
-const NotesPage = ({notes, setUpdateForm, handleClick}) => {
+export default function NotesPage({ notes, setNotes, setUpdateForm }) {
   return (
-    <div className='notesPage'>
+    <div>
       {notes.map(note => {
         return (
-            <Note key={note._id} note={note} setUpdateForm={setUpdateForm} handleClick={handleClick}/>
+          <Note 
+            key={note._id}
+            notes={notes} 
+            note={note} 
+            setUpdateForm={setUpdateForm} 
+            setNotes={setNotes}
+          />
         )
       })}
     </div>
   );
 }
-
-export default NotesPage
